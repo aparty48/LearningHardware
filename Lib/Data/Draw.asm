d3138048119ec6a24f0fb480bc5254081bd0e8c0
Aparty <47005928+aparty48@users.noreply.github.com>
2025-09-09 10:43:35 +0200

    Update Draw.asm
    Return constants

--- Lib/Data/Draw.asm
+++ Lib/Data/Draw.asm
@@ -7,12 +7,12 @@ DAE_FrameBufferSize   dq 0           ;Size BF
 DAE_HEX_Text          dq 0, 0        ;Text, 64 bit in format HEX, after converting
                       db 0
 DAE_Hello_World       db "Hello World!", 0
-DAE_Display_Symbols_Heigth    dq 91
-DAE_Display_Symbols_Width     dq 160
-DAE_Display_Heigth            dq 640
-DAE_Display_Width             dq 800
-DAE_Symbol_Height             dq 7
-DAE_Symbol_Width              dq 5
+DAE_Display_Symbols_Heigth    equ 91
+DAE_Display_Symbols_Width     equ 160
+DAE_Display_Heigth            equ 640
+DAE_Display_Width             equ 800
+DAE_Symbol_Height             equ 7
+DAE_Symbol_Width              equ 5
 
 DAE_Chars:
   %include "CharsTable.asm"
