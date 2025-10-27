02b253b16f64c965642e18fe482ccda30390c752
is7fd9xr <47005928+aparty48@users.noreply.github.com>
2025-10-27 21:42:18 +0100

    Advanced build script

--- build.sh
+++ build.sh
@@ -1,29 +1,77 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$0")
-cd "$SCRIPT_DIR"
+PROJECT_DIR=$(cd "$(dirname "$0")" && pwd)
+CONFIG_FILE="$PROJECT_DIR/Config.sh"
+EXIT_CODE=0
 
-nasm src/BOOTX64.asm -o out/EFI/BOOT/BOOTX64.EFI
-#nasm -f elf64 -g -F dwarf -o temp/Debug.o src/BOOTX64_DBG.asm
-#ld -T src/EFI_DBG.lds -o out/BOOTX64_DBG.elf temp/Debug.o
-#objcopy -j .text -j .data --target=pei-x86-64 "temp/BOOTX64_DBG.elf" -O "out/EFI/BOOT/BOOTX64.EFI"
-utils/checksumpe out/EFI/BOOT/BOOTX64.EFI
+# Check existing config file
+if [ ! -f "$CONFIG_FILE" ]; then
+	echo "ERROR: '$CONFIG_FILE' not found."
+	exit 1
+fi
+
+# Include config
+source $CONFIG_FILE
+
+# Check folders
+if [ ! -d "$PROJECT_DIR$TMP_DIR" ]; then mkdir -p "$PROJECT_DIR$TMP_DIR"; fi
+if [ ! -d "$PROJECT_DIR$MNT_DIR" ]; then mkdir -p "$PROJECT_DIR$MNT_DIR"; fi
+if [ ! -d "$PROJECT_DIR$OUT_DIR/EFI/BOOT" ]; then mkdir -p "$PROJECT_DIR$OUT_DIR/EFI/BOOT"; fi
+
+# Build BOOTX64.EFI
+if [ "$BOOTX64_EFI_BUILD_MODE" -eq 0 ]; then
+	echo "Build: BOOTX64.EFI ..."
+	"$NASM_BIN" "$PROJECT_DIR$BOOTX64_EFI_SRC_FILE" -o "$PROJECT_DIR$BOOTX64_EFI_OUT_FILE"
+	EXIT_CODE=$?
+	if [ "$EXIT_CODE" -eq 0 ]; then 
+		"$PROJECT_DIR$CHECKSUM_LOCAL_BIN" "$PROJECT_DIR$BOOTX64_EFI_OUT_FILE"
+		EXIT_CODE=$?
+		if [ "$EXIT_CODE" -eq 0 ]; then 
+			echo "Success." 
+		fi
+	fi
+fi
+
+# Build DEBUG BOOTX64.EFI
+if [ "$BOOTX64_EFI_BUILD_MODE" -eq 1 ]; then
+	echo "Build: Debug BOOTX64.ELF ..."
+	"$NASM_BIN" -f elf64 $BOOTX64_DBG_FLAGS "$PROJECT_DIR$BOOTX64_DBG_ELF_SRC_FILE" -o "$PROJECT_DIR$BOOTX64_DBG_O_TMP_FILE"
+	EXIT_CODE=$?
+	if [ "$EXIT_CODE" -eq 0 ]; then 
+		"$LD_BIN" -T "$PROJECT_DIR$BOOTX64_DBG_LD_SCRIPT" -o "$PROJECT_DIR$BOOTX64_DBG_ELF_TMP_FILE"
+		EXIT_CODE=$?
+		if [ "$EXIT_CODE" -eq 0 ]; then
+			"$OBJCOPY_BIN" -j .text -j data --target=efi-app-x86-64 "$PROJECT_DIR$BOOTX64_DBG_ELF_TMP_FILE" -O "$PROJECT_DIR$BOOTX64_EFI_OUT_FILE"
+			EXIT_CODE=$?
+			if [ "$EXIT_CODE" -eq 0 ]; then
+				echo "Success." 
+			fi
+		fi
+		
+	fi
+fi
+
+# Create .img file
+if [ "$ENABLE_CREATE_IMG_FILE" -eq 1 ]; then
+	dd if=/dev/zero of=$IMAGE_NAME bs=1024 count=$IMAGE_SIZE
+	mkfs.vfat $IMAGE_NAME
+	if grep -qs "$PROJECT_DIR$MNT_DIR" /proc/mounts; 
+	then sudo umount "$PROJECT_DIR$MNT_DIR"; fi
+	sudo mount "$IMAGE_NAME" "$PROJECT_DIR$MNT_DIR"
+	sudo cp -rf $PROJECT_DIR$OUT_DIR/* "$PROJECT_DIR$MNT_DIR/"
+	sudo umount "$PROJECT_DIR$MNT_DIR"
+	rm -rf "$PROJECT_DIR$MNT_DIR"
+fi
+
 
 #sudo apt install genisoimage
 #sudo apt install xorriso
 #genisoimage -o MyOS.iso -R -J -eltorito-alt-boot -no-emul-boot -efi-boot EFI/BOOT/BOOTX64.EFI Out
 #xorriso -as mkisofs -o MyOS.iso -isohybrid-mbr /usr/lib/syslinux/mbr/gptmbr.bin -c boot.catalog -b EFI/boot/bootx64.efi -no-emul-boot -eltorito-alt-boot -e EFI/boot/bootx64.efi -no-emul-boot -isohybrid-gpt-basdat Out
-dd if=/dev/zero of=OS.img bs=1024 count=100
-mkfs.vfat OS.img
-if grep -qs "/mnt" /proc/mounts; 
-then sudo umount /mnt; fi
-sudo mount OS.img /mnt
-sudo cp -rf out/* /mnt/
-sudo umount /mnt
 
 ##
 # qemu-system-x86_64 -drive if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/OVMF.fd -hda out/MyOS.img -enable-kvm -m 512 -serial stdio -gdb tcp::1111
 # qemu-system-x86_64 -drive if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/OVMF.fd -cdrom MyOS.iso -gdb tcp::1111 -S
 # gdb
 # target remote localhost:1111
-##
+##sudo
